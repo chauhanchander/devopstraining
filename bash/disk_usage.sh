338eda32be437acb6f469bcee586f4c271d61c52
Chander Mohan singh <chauhanchander27@gmail.com>
2025-08-28 12:04:38 +0530

    Update disk_usage.sh

--- bash/disk_usage.sh
+++ bash/disk_usage.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Lists all filesystems and flags the ones with 0% usage.
 
-output=`df -h | tail -n 7 | awk '{ print $3}'`
-for i in $output; do
-  if [ "$i" = "0" ]
-  then
-        echo "Found zero usage value = $i"
+# -P = POSIX output (stable columns, no wrapping)
+# Exclude common virtual filesystems if you only care about real disks: add
+#   -x tmpfs -x devtmpfs -x overlay -x squashfs
+
+df -P | awk 'NR>1 {
+  use = ($5 + 0)       # "3%" -> 3, "0%" -> 0
+  if (use == 0)
+    printf "Found zero usage on %-20s mounted at %-20s (Use=%s, Used=%s)\n", $1, $6, $5, $3
   else
-        echo "Not found zero value = $i"
-  fi
-done
+    printf "Not zero      on %-20s mounted at %-20s (Use=%s, Used=%s)\n", $1, $6, $5, $3
+}'
